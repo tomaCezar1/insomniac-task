23a8ea3afa87a499fba9d72a3074aaa8b9bffe88
Cezar <cezar.toma1@gmail.com>
2022-02-28 17:26:56 +0200

    feat: add mockData

---
+++ src/entities/mockData.ts
@@ -0,0 +1,11 @@
+export interface MockData {
+  heading: string;
+  subheading: string;
+  content: string;
+}
+
+export interface ContactData {
+  title: string;
+  details: string;
+  email: string;
+}
---
+++ src/mocks/mockData.ts
@@ -0,0 +1,16 @@
+import { ContactData, MockData } from 'entities/mockData';
+
+export const messageData: MockData = {
+  heading: 'IDS’s Message',
+  subheading:
+    'Knowledge management has always been intertwined with the work of Insomniac Design Study (IDS).',
+  content:
+    'Sed aliquet eleifend dapibus. Aliquam erat volutpat. Aliquam erat volutpat. Curabitur ante lorem, vehicula ut lacinia cursus, posuere nec turpis. Praesent bibendum turpis ut leo porttitor, sit amet tempor turpis convallis. Nullam nec pharetra turpis, sed elementum arcu. Nullam venenatis sit amet justo id euismod. Nulla dictum ligula commodo venenatis luctus. Duis at enim suscipit, luctus nunc eu, varius lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris eu lacus congue, lacinia leo non, facilisis tortor. Nam ultricies enim at dolor porta, at malesuada lorem luctus. ',
+};
+
+export const contactData: ContactData = {
+  title: 'Lord Voldemort',
+  details:
+    'Vice-President for Knowledge Management and Sustainable Development Activities',
+  email: 'lordVoldemort@example.com',
+};
